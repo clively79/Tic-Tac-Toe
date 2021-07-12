b1fd1733c958065882975d349cab07219cccf279
Christopher Lively <christopher.lively@unlv.edu>
2021-07-12 16:08:05 -0700

    Added Documentation

--- Space.py
+++ Space.py
@@ -1,6 +1,13 @@
 class Space():
+    """
+        A Python class representing a single space of a game board.  
+        _value = 0 unocupied
+        _value_ = 1 occupied
+    """
 
     def __init__(self):
+        """Constructor method
+        """
         self._value = 0
 
     def __repr__(self):
@@ -10,10 +17,19 @@ class Space():
         return str(self._value)
 
     def set(self):
+        """set _value to 1 (occupied)
+        """
         self._value = 1
 
     def reset(self):
+        """Set's _value to 0 (unoccupied)
+        """
         self._value = 0
 
     def get(self):
+        """getter method
+
+        Returns:
+            Integer: returns  _value
+        """
         return self._value
