b610e5381e234d2b83ea9460d09b2bf67cf72c8f
Christopher Lively <christopher.lively@unlv.edu>
2021-07-12 15:55:00 -0700

    removed printBoard method, added documentation

--- Board.py
+++ Board.py
@@ -3,9 +3,20 @@ from math import sqrt
 
 
 class Board():
+    """
+        A class used to represent a square two dimential game board.
+        A player's board consists for a list of Space objects in row major
+        order.  each space may contain a value of 0 if unocupied and 1 if 
+        occupied.
+    """
 
     def __init__(self, n):
-        self.p = int(sqrt(n))
+        """Constructor
+
+        Args:
+            n (Integer): number of rows
+        """
+        self.p = n
         self.sp = []
         self.row = []
         self.col = []
@@ -28,17 +39,31 @@ class Board():
     def __str__(self):
         return str(self.sp)
 
-    def printBoard(self):
-        print(*self.sp, sep="")
-
     def set(self, n):
+        """ Setter method, changes the state of the desired space from 0 (unoccupied)
+            to 1 (occupied).
+
+        Args:
+            n (Integer): Index of the space chosen by the player
+        """
         self.sp[n].set()
 
-    def reset(self, n):
-        for i in range(0, n):
+    def reset(self):
+        """Resets all the player's spaces to 0 (unoccupied)
+
+        """
+        for i in range(0, self.p**2):
             self.sp[i].reset()
 
-    def binToDecimal(self, s):
+    def _binToDecimal(self, s):
+        """Helper method: Accepts a binary string converts decimal value
+
+        Args:
+            s (String): Binary coded decimal
+
+        Returns:
+            Integer: integer value omputed from binary coded decimal s
+        """
         dval = 0
         rlist = list(s)
         rlist.reverse()
@@ -50,9 +75,13 @@ class Board():
         return dval
 
     def winner(self):
+        """Determines if the player's board contains a winning row, column, or diagonal 
 
+        Returns:
+            Tuple: (Boolean, integer)
+        """
         for i in range(len(self.row)):
-            if self.binToDecimal(''.join(map(str, self.row[i]))) == 7:
+            if self._binToDecimal(''.join(map(str, self.row[i]))) == 7:
                 return True, i
 
         for i in range(len(self.col)):
@@ -64,25 +93,3 @@ class Board():
                 return True, i+(2 * self.p)
 
         return False, None
-
-
-# def main():
-#     board = Board(9)
-#     board.set(0)
-#     # board.set(1)
-#     # board.set(2)
-#     # board.set(3)
-#     board.set(4)
-#     # board.set(5)
-#     # board.set(6)
-#     # board.set(7)
-#     board.set(8)
-
-#     winner = board.winner()
-#     if winner[0]:
-#         print(f"winner found on {winner[1]}")
-#     board.reset(9)
-#     board.printBoard()
-
-
-# main()
